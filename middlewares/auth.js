f931374fe8e91ba35bd8f19aa5c3aace7f16cbd6
Aleksey Shlyapnikov <aleksdsgn@ya.ru>
2022-11-07 20:48:07 +0600

    feat: create middleware for authorization

---
+++ middlewares/auth.js
@@ -0,0 +1,31 @@
+import jwt from 'jsonwebtoken';
+
+export const auth = (req, res, next) => {
+  // достаём авторизационный заголовок
+  const { authorization } = req.headers;
+
+  // убеждаемся, что он есть или начинается с Bearer
+  if (!authorization || !authorization.startWith('Bearer ')) {
+    return res
+      .status(401)
+      .send({ message: 'Необходима авторизация' });
+  }
+
+  // извлечём токен
+  const token = authorization.replace('Bearer ', '');
+  let payload;
+
+  try {
+    // пытаемся верифицировать токен
+    payload = jwt.verify(token, 'some-secret-key');
+  } catch (err) {
+    // отправим ошибку, если не получилось
+    return res
+      .status(401)
+      .send({ message: 'Необходима авторизация' });
+  }
+  // записываем пейлоуд в объект запроса
+  req.user = payload;
+
+  next();
+};
