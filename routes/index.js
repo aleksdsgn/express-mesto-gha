3d7c5d7f4b15c9819fe3704f21da84445dac988e
Aleksey Shlyapnikov <aleksdsgn@ya.ru>
2022-11-20 12:11:23 +0700

    feat: add a signout router

--- routes/index.js
+++ routes/index.js
@@ -19,6 +19,10 @@ router.post('/signup', celebrateBodyUser, register);
 router.use('/users', auth, userRouter);
 router.use('/cards', auth, cardRouter);
 
+router.get('/signout', (req, res) => {
+  res.clearCookie('jwt').send({ message: 'Выход' });
+});
+
 // обработка неправильного пути
 router.use('*', auth, (req, res, next) => {
   next(new NotFoundError('Путь не найден'));
